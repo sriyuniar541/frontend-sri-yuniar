a5fa6880e78405a0c8be0064481e3d82e36d13c2
sri yuniar <sriyuniar541@gmail.com>
2024-07-08 23:52:40 +0900

    update

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ export default function Navbar() {
       <div>
         <nav className="container navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="/">Navbar</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
